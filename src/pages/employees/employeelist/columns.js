ecc964b7f34cf127529c3383451f12b7928d901e
Poonam-Gorai <poonamgorai1807@gmail.com>
2021-10-28 15:21:13 +0530

    commit

---
+++ src/pages/employees/employeelist/columns.js
@@ -0,0 +1,29 @@
+export const COLUMNS = [
+    {
+      accessor:"image"  
+    },
+  {
+    Header: "ID",
+    accessor: "id"
+  },
+  {
+    Header: "NAME/EMAIL",
+    accessor:"ename,email"
+  },
+  {
+    Header: "ROLE",
+    accessor:"role"
+  },
+  {
+    Header: "MOBILE",
+    accessor:"mobile"
+  },
+  {
+    Header: "JOIN DATE",
+    accessor:"join"
+  },
+  {
+    Header: "MANAGER",
+    accessor:"manager"
+  },
+];
--- src/pages/employees/employees.js
+++ src/pages/employees/employees.js
@@ -2,14 +2,42 @@ import React, { useState } from "react";
 import "./employees.css";
 import SearchIcon from "../../assets/images/search-icon.png";
 import data from "../../redux/store/data";
-import ReactPaginate from "react-paginate";
+import TableScrollbar from "react-table-scrollbar";
 
 function Employees() {
   const [emplist, setEmpList] = useState(data);
 
+  //search
+  const [searchTerm, setSearchTerm] = useState("");
 
+  //Sorting
+  const [order, setOrder] = useState("ASC");
 
-  
+  const sorting = (col) => {
+    console.log("clicked");
+    if (order === "ASC") {
+      const sorted = [...emplist].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      setEmpList(sorted);
+      setOrder("DSC");
+    }
+    if (order === "DSC") {
+      const sorted = [...emplist].sort((a, b) =>
+        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      );
+      setEmpList(sorted);
+      setOrder("ASC");
+    }
+  };
+
+  // pagination
+  const [visible, setVisible] = useState(10);
+  const showMoreItems = () => {
+    console.log("click")
+    setVisible((prevValue) => prevValue + 10);
+  };
+
   return (
     <article className="emp_article">
       <div className="Breadcrums">
@@ -23,42 +51,82 @@ function Employees() {
           <input
             type="text"
             className="searchfield"
-            placeholder="Search by Employee ID,Name,email"
+            placeholder="Search by Employee Name,email and role"
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
           ></input>
         </div>
       </div>
       <div className="emp-table">
-        <table>
-          <thead>
-            <tr className="row-head">
-              <th className="empid-head">EMP. ID</th>
-              <th className="empname-head">NAME/EMAIL</th>
-              <th className="emprole-head">ROLE</th>
-              <th className="empmobile-head">MOBILE</th>
-              <th className="empjoin-head">JOIN DATE</th>
-              <th className="empmanager-head">MANAGER</th>
-            </tr>
-          </thead>
-          <div className="emp-list">
+      <TableScrollbar>
+          <table>
+            <thead>
+              <tr className="row-head">
+                <th onClick={() => sorting("id")} className="empid-head">
+                  EMP. ID
+                </th>
+                <th onClick={() => sorting("ename")} className="empname-head">
+                  NAME/EMAIL
+                </th>
+                <th onClick={() => sorting("role")} className="emprole-head">
+                  ROLE
+                </th>
+                <th className="empmobile-head">MOBILE</th>
+                <th className="empjoin-head">JOIN DATE</th>
+                <th className="empmanager-head">MANAGER</th>
+              </tr>
+            </thead>
+            {/* <div className="emp-list"> */}
+            
             <tbody>
-              {emplist.map((val, index) => (
-                <tr className="emprow" key={index}>
-                  <td className="empid">
-                    <img src={val.image} alt="pro" className="emp-img" />#{val.id}
-                  </td>
-                  <td className="empname-email">
-                    <div className="ename">{val.ename}</div>
-                    <div className="emp-email">{val.email}</div>
-                  </td>
-                  <td className="emp-item">{val.role}</td>
-                  <td className="emp-item1">{val.mobile}</td>
-                  <td className="emp-item1">{val.join}</td>
-                  <td className="emp-manager">{val.manager}</td>
-                </tr>
-              ))}
+              {emplist
+              // eslint-disable-next-line
+                .filter((val) => {
+                  if (searchTerm === "") {
+                    return val;
+                  } else if (
+                    val.ename.toLowerCase().includes(searchTerm.toLowerCase())
+                  ) {
+                    return val;
+                  } else if (
+                    val.email.toLowerCase().includes(searchTerm.toLowerCase())
+                  ) {
+                    return val;
+                  } else if (
+                    val.role.toLowerCase().includes(searchTerm.toLowerCase())
+                  ) {
+                    return val;
+                  } else if (
+                    val.id.toLowerCase().includes(searchTerm.toLowerCase())
+                  ) {
+                    return val;
+                  }
+                })
+                .slice(0, visible)
+                .map((val, index) => (
+                  <tr className="emprow" key={index}>
+                    <td className="empid">
+                      <img src={val.image} alt="img" className="emp-img" />#
+                      {val.id}
+                    </td>
+                    <td className="empname-email">
+                      <div className="ename">{val.ename}</div>
+                      <div className="emp-email">{val.email}</div>
+                    </td>
+                    <td className="emp-item">{val.role}</td>
+                    <td className="emp-item1">{val.mobile}</td>
+                    <td className="emp-item1">{val.join}</td>
+                    <td className="emp-manager">{val.manager}</td>
+                  </tr>
+                ))}
             </tbody>
+            {/* </div> */}
+          </table>
+          <div className="btn-See-more">
+            <button onClick={showMoreItems} className="btnSee">See more...</button>
           </div>
-        </table>
+          </TableScrollbar>
       </div>
       <footer className="footer">
         <div className="footer_btn">
